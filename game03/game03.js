74d4e182fb2a46bef17863b34f7f9089d64dc15f
BigPorojects <djamilaagro@gmail.com>
2026-09-21 16:44:41 +0100

    Update game03.js

--- game03/game03.js
+++ game03/game03.js
@@ -1,6 +1,6 @@
 // بيانات الدرس والكلمات
 const gameData = {
-    // الكلمات الصحيحة للفراغات بالترتيب من 0 إلى 12 (إجمالي 13 فراغاً)
+    // الكلمات الصحيحة للفراغات بالترتيب من 0 إلى 12 (إجمالي 13 فراغاً بدون تكرار)
     correctAnswers: [
         "supplies",
         "book",
@@ -9,7 +9,6 @@ const gameData = {
         "glue",
         "tape",
         "pencil case",
-        "pencil case",
         "pencils",
         "pens",
         "eraser",
@@ -18,7 +17,7 @@ const gameData = {
         "color pencils"
     ],
 
-    // الخيارات التي ستظهر للاعب في النافذة المنبثقة (13 خياراً)
+    // الخيارات الفريدة التي ستظهر للاعب في النافذة المنبثقة
     baseWordOptions: [
         "supplies",
         "book",
@@ -27,7 +26,6 @@ const gameData = {
         "glue",
         "tape",
         "pencil case",
-        "pencil case",
         "pencils",
         "pens",
         "eraser",
@@ -36,7 +34,7 @@ const gameData = {
         "color pencils"
     ],
 
-    // نص الفقرة الرئيسي مع ترقيم الجمل وتمرير 13 فراغاً فقط
+    // نص الفقرة الرئيسي مدمج في 12 عبارة مرقمة
     paragraphHTML: `
         1. School <span class="blank" data-index="0"></span>.<br><br>
         2. It's my <span class="blank" data-index="1"></span>. I can read.<br><br>
@@ -44,12 +42,11 @@ const gameData = {
         4. These are my <span class="blank" data-index="3"></span>. I can cut the paper.<br><br>
         5. It's my <span class="blank" data-index="4"></span>. I can stick them with it.<br><br>
         6. It's my <span class="blank" data-index="5"></span>. I can put them with it.<br><br>
-        7. Is this your <span class="blank" data-index="6"></span>? Yes, it is.<br><br>
-        8. What's in your <span class="blank" data-index="7"></span>?<br><br>
-        9. These are my <span class="blank" data-index="8"></span> and <span class="blank" data-index="9"></span>. I like to write.<br><br>
-        10. It's my <span class="blank" data-index="10"></span>. I can erase and write again.<br><br>
-        11. It's my <span class="blank" data-index="11"></span>. I can mark with it.<br><br>
-        12. It is my <span class="blank" data-index="12"></span>. I like to measure.<br><br>
-        13. These are my <span class="blank" data-index="13"></span>. I can draw a rainbow.
+        7. Is this your <span class="blank" data-index="6"></span>? Yes, it is. What's in your pencil case?<br><br>
+        8. These are my <span class="blank" data-index="7"></span> and <span class="blank" data-index="8"></span>. I like to write.<br><br>
+        9. It's my <span class="blank" data-index="9"></span>. I can erase and write again.<br><br>
+        10. It's my <span class="blank" data-index="10"></span>. I can mark with it.<br><br>
+        11. It is my <span class="blank" data-index="11"></span>. I like to measure.<br><br>
+        12. These are my <span class="blank" data-index="12"></span>. I can draw a rainbow.
     `
 };
