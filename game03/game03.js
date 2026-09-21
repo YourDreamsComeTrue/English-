e600e18335fefc89531a9ff7d0836f6db88bf4e0
BigPorojects <djamilaagro@gmail.com>
2026-09-21 16:17:16 +0100

    Update game03.js

--- game03/game03.js
+++ game03/game03.js
@@ -1,61 +1,55 @@
 // بيانات الدرس والكلمات
 const gameData = {
-    // الكلمات الصحيحة للفراغات بالترتيب من 0 إلى 15 (إجمالي 16 فراغاً)
+    // الكلمات الصحيحة للفراغات بالترتيب من 0 إلى 12 (إجمالي 13 فراغاً)
     correctAnswers: [
         "supplies",
-        "backpack",
         "book",
         "notebook",
         "scissors",
         "glue",
-        "pencil case",
         "tape",
-        "backpack",
+        "pencil case",
+        "pencil case",
         "pencils",
         "pens",
         "eraser",
         "highlighter",
         "ruler",
-        "color",
-        "pencils"
+        "color pencils"
     ],
 
-    // الخيارات التي ستظهر للاعب في النافذة المنبثقة (تتضمن 16 كلمة مطابقة للإجابات المطلوبة)
+    // الخيارات التي ستظهر للاعب في النافذة المنبثقة (13 خياراً)
     baseWordOptions: [
-        "supplies", 
-        "backpack", 
-        "book", 
+        "supplies",
+        "book",
         "notebook",
-        "scissors", 
-        "glue", 
-        "pencil case", 
+        "scissors",
+        "glue",
         "tape",
-        "backpack", 
-        "pencils", 
-        "pens", 
+        "pencil case",
+        "pencil case",
+        "pencils",
+        "pens",
         "eraser",
-        "highlighter", 
-        "ruler", 
-        "color", 
-        "pencils"
+        "highlighter",
+        "ruler",
+        "color pencils"
     ],
 
-    // نص الفقرة الرئيسي مع الفراغات (من 0 إلى 15)
+    // نص الفقرة الرئيسي مع ترقيم الجمل وتمرير 13 فراغاً فقط
     paragraphHTML: `
-        School <span class="blank" data-index="0"></span><br><br>
-        What's in your <span class="blank" data-index="1"></span>?<br>
-        It's my <span class="blank" data-index="2"></span>. I can read.<br>
-        It's my <span class="blank" data-index="3"></span>. I can write on it.<br>
-        These are my <span class="blank" data-index="4"></span>. I can cut the paper.<br>
-        It's my <span class="blank" data-index="5"></span>. I can stick them with it.<br>
-        It's my <span class="blank" data-index="6"></span>. I can put them with it.<br>
-        Is this your <span class="blank" data-index="7"></span>? Yes, it is.<br><br>
-        What's in your <span class="blank" data-index="8"></span>?<br>
-        These are my <span class="blank" data-index="9"></span> and <span class="blank" data-index="10"></span>. I like to write.<br>
-        It's my <span class="blank" data-index="11"></span>. I can erase and write again.<br>
-        It's my <span class="blank" data-index="12"></span>. I can mark with it.<br>
-        It is my <span class="blank" data-index="13"></span>. I like to measure.<br>
-        These are my <span class="blank" data-index="14"></span> <span class="blank" data-index="15"></span>. I can draw a rainbow.<br><br>
-        There are things for school in my backpack.
+        1. School <span class="blank" data-index="0"></span>.<br><br>
+        2. It's my <span class="blank" data-index="1"></span>. I can read.<br><br>
+        3. It's my <span class="blank" data-index="2"></span>. I can write on it.<br><br>
+        4. These are my <span class="blank" data-index="3"></span>. I can cut the paper.<br><br>
+        5. It's my <span class="blank" data-index="4"></span>. I can stick them with it.<br><br>
+        6. It's my <span class="blank" data-index="5"></span>. I can put them with it.<br><br>
+        7. Is this your <span class="blank" data-index="6"></span>? Yes, it is.<br><br>
+        8. What's in your <span class="blank" data-index="7"></span>?<br><br>
+        9. These are my <span class="blank" data-index="8"></span> and <span class="blank" data-index="9"></span>. I like to write.<br><br>
+        10. It's my <span class="blank" data-index="10"></span>. I can erase and write again.<br><br>
+        11. It's my <span class="blank" data-index="11"></span>. I can mark with it.<br><br>
+        12. It is my <span class="blank" data-index="12"></span>. I like to measure.<br><br>
+        13. These are my <span class="blank" data-index="13"></span>. I can draw a rainbow.
     `
 };
